961a4cdf3b8d819aedc3cda937bc07652a7da4f9
Juan Ignacio Molina <juanignaciomln@gmail.com>
2021-06-24 02:28:40 -0300

    Partially implemented api/preference and api/notifications

--- pages/api/notifications.js
+++ pages/api/notifications.js
@@ -1,3 +1,13 @@
+import { supabase } from "../../supabase";
+
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  // Return ASAP to avoid a timeout in MercadoPago
+  res.status(200);
+
+  supabase.from("notifications").insert(
+    { notification: req.body },
+    {
+      returning: "minimal", // Don't return the value after inserting
+    }
+  );
 }
--- pages/api/preference.js
+++ pages/api/preference.js
@@ -1,3 +1,75 @@
+import { supabase } from "../../supabase";
+import { MERCADOPAGO_ACCESS_TOKEN } from "../../configuration";
+import mercadopago from "mercadopago";
+
+const PUBLIC_URL = "https://mp-certification.vercel.app";
+
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  mercadopago.configure({
+    access_token: MERCADOPAGO_ACCESS_TOKEN,
+  });
+
+  // PREFERENCE DATA HARDCODED AS DESCRIBED BY THE CHALLANGE SPECS
+  const preference = {
+    items: [
+      {
+        id: "1234",
+        title: "Apple iPhone 12 Pro Max 256GB",
+        description: "Dispositivo móvil de Tienda e-commerce",
+        unit_price: 350000,
+        quantity: 1,
+        picture_url: `${PUBLIC_URL}/images/iphone_12.jpeg`,
+        external_reference: "juanimolina92@hotmail.com",
+      },
+    ],
+    payer: {
+      email: "test_user_63274575@testuser.com",
+      name: "Lalo",
+      surname: "Landa",
+      phone: {
+        area_code: "11",
+        number: "22223333",
+      },
+      address: {
+        street_name: "Falsa",
+        street_number: "123",
+        zip_code: "1111",
+      },
+    },
+    excluded_payment_methods: [
+      {
+        id: "amex",
+      },
+    ],
+    excluded_payment_types: [
+      {
+        id: "atm",
+      },
+    ],
+    auto_return: "approved",
+    installments: 6,
+    back_urls: {
+      success: `${PUBLIC_URL}/payment/success`,
+      failure: `${PUBLIC_URL}/payment/failure`,
+      pending: `${PUBLIC_URL}/payment/pending`,
+    },
+    notification_url: `${PUBLIC_URL}/api/notifications?source_news=webhooks`,
+  };
+
+  mercadopago.preferences
+    .create(preference)
+    .then((response) =>
+      supabase
+        .from("preferences")
+        .insert(
+          { preference: response.body },
+          {
+            returning: "minimal", // Don't return the value after inserting
+          }
+        )
+        .then(() => Promise.resolve(response))
+    )
+    .then((response) => {
+      res.status(200).json({ preference: response.body });
+    });
 }
