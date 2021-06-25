efb9d03794e42763afeeed8de13b113ce243395d
Juan Ignacio Molina <juanignaciomln@gmail.com>
2021-06-25 20:38:50 -0300

    Improvements on notifications endpoint

--- pages/api/notifications.js
+++ pages/api/notifications.js
@@ -3,10 +3,38 @@ import { supabase } from "../../supabase";
 export default function handler(req, res) {
   res.status(200);
 
-  supabase.from("notifications").insert(
-    { notification: req.body },
-    {
-      returning: "minimal", // Don't return the value after inserting
-    }
-  );
+  const notification = req.body;
+  const paymentId = notification.data.id;
+
+  console.log(notification);
+  console.log(paymentId);
+
+  fetch(`https://api.mercadopago.com/v1/payments/${paymentId}`, {
+    method: "get",
+    headers: new Headers({
+      Authorization: `Bearer ${mercadoPagoAccessToken}`,
+    }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject(
+          new functions.https.HttpsError(
+            "unknown",
+            `Can't retrieve information from MercadoPago for payment with ID: ${paymentId}`
+          )
+        );
+      }
+    })
+    .then((paymentData) =>
+      supabase.from("notifications").insert(
+        { notification: notification, payment: paymentData },
+        {
+          returning: "minimal", // Don't return the value after inserting
+        }
+      )
+    );
+
+  return Promise.resolve();
 }
